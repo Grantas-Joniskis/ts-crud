35daaefd0ac71b5b3a92d21b3a609f7a1f0a07f8
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-24 18:23:08 +0200

    feat(5): adding consturctor to table component.

--- src/components/table.ts
+++ src/components/table.ts
@@ -3,3 +3,21 @@ export type TableProps<Type> = {
   columns: Type,
   rowsData: Type[],
 };
+
+class Table {
+  public htmlElement: HTMLTableElement;
+
+  private props: TableProps<Type>;
+
+  private tbody: HTMLTableSectionElement;
+
+  private thead: HTMLTableSectionElement;
+
+  public constructor(props: TableProps<Type>) {
+    this.props = props;
+    this.htmlElement = document.createElement('table');
+    this.thead = document.createElement('thead');
+    this.tbody = document.createElement('tbody');
+
+  }
+}
