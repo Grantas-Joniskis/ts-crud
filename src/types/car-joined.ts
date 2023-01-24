0a2a17d06871757e918f9fe8b10b6763898a1346
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-24 18:06:15 +0200

    feat(2): Creating cars collection.

--- src/types/car-joined.ts
+++ src/types/car-joined.ts
@@ -1,7 +1,11 @@
-type CarJoined = {
-  id: string,
-  title: string,
-  brandId: string,
+
+import Brand from './brand';
+import Model from './model';
+import Car from './car';
+
+type CarJoined = Omit<Car, 'modelId'> & {
+  brand: Brand['title'],
+  model: Model['title'],
 };
 
 export default CarJoined;
