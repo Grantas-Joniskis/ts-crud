89888906a8a95748b6a005a886f38427f8def756
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-24 18:15:25 +0200

    feat(3): creating app component.

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,8 +1,16 @@
+import cars from "../data/cars";
+import brands from "../data/brands";
+import models from "../data/models";
+import CarsCollection from '../helpers/cars-collection';
+
 class App {
   private htmlElement: HTMLElement;
 
+  private carsCollection: CarsCollection;
+
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
+    this.carsCollection = new CarsCollection({ cars, brands, models });
 
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
@@ -10,11 +18,7 @@ class App {
   }
 
   initialize = (): void => {
-    const container = document.createElement('div');
-    container.className = 'container my-5';
-    container.innerHTML = 'Laukiu kol būsiu sukurtas';
 
-    this.htmlElement.append(container);
   };
 }
 
