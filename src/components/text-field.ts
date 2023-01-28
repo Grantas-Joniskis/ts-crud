099929cbe39acf78bfaaf55f13d8bf3ef24493d5
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-28 16:47:28 +0200

    feat(17): adding initialize() to text-field.ts

--- src/components/text-field.ts
+++ src/components/text-field.ts
@@ -13,8 +13,34 @@ class TextField {
 
   private props: TextFieldProps;
 
+  private htmlInputElement: HTMLInputElement;
+
+  private htmlLabelElement: HTMLLabelElement;
+
+  public htmlElement: HTMLDivElement;
+
   public constructor(props: TextFieldProps) {
     TextField.instanceCounter += 1;
     this.props = props;
+
+    this.htmlElement = document.createElement('div');
+    this.htmlLabelElement = document.createElement('label');
+    this.htmlInputElement = document.createElement('input');
   }
+
+  private initialize = (): void => {
+    const inputId = `input-${TextField.id}`;
+
+    this.htmlLabelElement.setAttribute('for', inputId);
+    this.htmlLabelElement.className = 'form-label';
+
+    this.htmlInputElement.id = inputId;
+    this.htmlInputElement.className = 'form-control';
+    this.htmlInputElement.type = 'text';
+
+    this.htmlElement.append(
+      this.htmlLabelElement,
+      this.htmlInputElement,
+    );
+  };
 }
