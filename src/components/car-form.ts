8b15ebb126e114d19b08698dd24d8d002fd10f98
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-28 17:08:12 +0200

    feat(23): adding initilialize()

--- src/components/car-form.ts
+++ src/components/car-form.ts
@@ -1,3 +1,8 @@
+import TextField from './text-field';
+import SelectField from './select-field';
+import brands from '../data/brands';
+import models from '../data/models';
+
 export type Values = {
   brand: string,
   model: string,
@@ -12,6 +17,13 @@ type CarFormProps = {
   onSubmit: (values: Values) => void,
 };
 
+type Fields = {
+  brand: SelectField,
+  model: SelectField,
+  price: TextField,
+  year: TextField,
+};
+
 class CarForm {
   public htmlElement: HTMLFormElement;
 
@@ -23,6 +35,8 @@ class CarForm {
 
   private htmlSubmitBtn: HTMLButtonElement;
 
+  private fields: Fields;
+
   public constructor(props: CarFormProps) {
     this.props = props;
 
@@ -30,5 +44,43 @@ class CarForm {
     this.htmlFieldsContainer = document.createElement('div');
     this.htmlFormHeader = document.createElement('h2');
     this.htmlSubmitBtn = document.createElement('button');
+
+    this.fields = {
+      brand: new SelectField({
+        name: 'brand',
+        labelText: 'Markė',
+        options: brands.map(({ id, title }) => ({ title, value: id })),
+      }),
+      model: new SelectField({
+        name: 'model',
+        labelText: 'Modelis',
+        options: models.map(({ id, title }) => ({ title, value: id })),
+      }),
+      price: new TextField({
+        name: 'price',
+        labelText: 'Kaina',
+      }),
+      year: new TextField({
+        name: 'year',
+        labelText: 'Metai',
+      }),
+    };
   }
+
+  private initialize = (): void => {
+    this.htmlFormHeader.className = 'h3 text-center';
+
+    const fieldsArr = Object.values(this.fields);
+    this.htmlFieldsContainer.className = 'd-flex flex-column gap-2';
+    this.htmlFieldsContainer.append(...fieldsArr.map((field) => field.htmlElement));
+
+    this.htmlSubmitBtn.className = 'btn btn-primary';
+
+    this.htmlElement.className = 'card d-flex flex-column gap-3 p-3';
+    this.htmlElement.append(
+      this.htmlFormHeader,
+      this.htmlFieldsContainer,
+      this.htmlSubmitBtn,
+    );
+  };
 }
