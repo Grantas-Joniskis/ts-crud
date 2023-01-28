666e5f9da10c6a123979345488a565d5cfe363aa
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-28 17:19:34 +0200

    feat(25): creating a new car in app.ts

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -9,22 +9,28 @@ type CarsCollectionProps = {
   models: Model[],
 };
 
-class CarsCollection {
-  private props: CarsCollectionProps;
+export type CarProps = {
+  brandId: string,
+  modelId: string,
+  price: number,
+  year: number
+};
 
-  constructor(props: CarsCollectionProps) {
-    this.props = props;
-  }
+const createId = (): string => String(Math.floor(Math.random() * 100000000000000));
+
+class CarsCollection {
+  constructor(private props: CarsCollectionProps) { }
 
   private joinCar = ({ modelId, ...car }: Car) => {
     const { brands, models } = this.props;
+
     const carModel = models.find((model) => model.id === modelId);
     const carBrand = brands.find((brand) => brand.id === carModel?.brandId);
 
     return {
       ...car,
-      brand: (carBrand && carBrand.title) ?? 'unknown',
-      model: (carModel && carModel.title) ?? 'unknown',
+      brand: (carBrand && carBrand?.title) ?? 'unknown',
+      model: (carModel && carModel?.title) ?? 'unknown',
     };
   };
 
@@ -48,6 +54,24 @@ class CarsCollection {
 
   public deleteCarById = (carId: string): void => {
     this.props.cars = this.props.cars.filter((car) => car.id !== carId);
+  };
+
+  public add = ({ modelId, brandId, ...carProps }: CarProps): void => {
+    const { models, brands, cars } = this.props;
+    const model = models.find((m) => m.id === modelId);
+    const brand = brands.find((b) => b.id === brandId);
+
+    if (!model || !brand) {
+      throw new Error('Netinkami duomenys sukurti automobilį');
+    }
+
+    const newCar: Car = {
+      id: createId(),
+      ...carProps,
+      modelId,
+    };
+
+    cars.push(newCar);
   };
 }
 
