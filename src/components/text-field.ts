14e05852c60550e28b8be0174df7ce780a6924c5
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-28 16:45:09 +0200

    feat(16): adding unique id to TextField.

--- src/components/text-field.ts
+++ src/components/text-field.ts
@@ -3,3 +3,18 @@ export type TextFieldProps = {
   labelText: string,
   value?: string,
 };
+
+class TextField {
+  private static instanceCounter = 0;
+
+  private static get id() {
+    return `${this.name}-${this.instanceCounter}`;
+  }
+
+  private props: TextFieldProps;
+
+  public constructor(props: TextFieldProps) {
+    TextField.instanceCounter += 1;
+    this.props = props;
+  }
+}
