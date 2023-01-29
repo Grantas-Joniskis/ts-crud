37841ff8359cd0598a561e7b131b0ee3e9dc45b1
Grantas-Joniskis <grantasjo@gmail.com>
2023-01-29 12:18:05 +0200

    feat(26): adding edit button

--- src/components/table.ts
+++ src/components/table.ts
@@ -9,7 +9,9 @@ export type TableProps<Type> = {
   title: string,
   columns: Type,
   rowsData: Type[],
+  editedCarId: string | null,
   onDelete: (id: string) => void,
+  onEdit: (id: string) => void,
 };
 
 class Table<Type extends RowData> {
@@ -78,11 +80,16 @@ class Table<Type extends RowData> {
   };
 
   private renderBodyView = (): void => {
-    const { rowsData, columns } = this.props;
+    const { rowsData, columns, editedCarId } = this.props;
 
     this.tbody.innerHTML = '';
     const rowsHtmlElements = rowsData
       .map((rowData) => {
+        const tr = document.createElement('tr');
+        if (editedCarId === rowData.id) {
+          tr.style.backgroundColor = '#fff2cf';
+        }
+
         const rowHtmlElement = document.createElement('tr');
 
         const cellsHtmlString = Object.keys(columns)
@@ -100,9 +107,17 @@ class Table<Type extends RowData> {
   };
 
   private addActionsCell = (rowHtmlElement: HTMLTableRowElement, id: string): void => {
-    const { onDelete } = this.props;
+    const { onDelete, onEdit, editedCarId } = this.props;
 
     const buttonCell = document.createElement('td');
+
+    const isCancelButton = editedCarId === id;
+    const editButton = document.createElement('button');
+    editButton.type = 'button';
+    editButton.innerHTML = isCancelButton ? 'Cancel' : 'Edit';
+    editButton.className = `btn btn-${isCancelButton ? 'dark' : 'warning'}`;
+    editButton.style.width = '80px';
+    editButton.addEventListener('click', () => onEdit(id));
 
     const deleteButton = document.createElement('button');
     deleteButton.type = 'button';
